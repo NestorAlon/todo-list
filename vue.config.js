d8abd4ede53d6c5f523f9f8ae3e34c59ebfc7a0f
Nestor Alonso <nestor1alon@gmail.com>
2024-10-31 19:27:53 -0300

    f

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV-- - "production" ? "/todo-list/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/todo-list/" : "/",
 });
